70ab78cea22ce8482232c103ab969ff8557338a0
sherif-med <sherif-med@users.noreply.github.com>
2021-06-17 10:50:13 +0000

    [ci skip] adding github actions files & updating README and Dockerfile f43b3c8e4e41d5ddfffed27b0aac0c3277f7fb0e

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['cpptemplate_43',['cpptemplate',['../index.html',1,'']]],
-  ['contributor_20covenant_20code_20of_20conduct_44',['Contributor Covenant Code of Conduct',['../md_CODE_OF_CONDUCT.html',1,'']]]
+  ['contributor_20covenant_20code_20of_20conduct_43',['Contributor Covenant Code of Conduct',['../md_CODE_OF_CONDUCT.html',1,'']]],
+  ['cpptemplate_44',['cpptemplate',['../index.html',1,'']]]
 ];
